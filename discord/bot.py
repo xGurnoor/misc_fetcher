942f663887a94ca442023a9333d326595e3365b1
Gurnoor Singh <44425264+xGurnoor@users.noreply.github.com>
2024-08-24 11:20:15 +0530

    fix: use correct venv for subprocess

--- discord/bot.py
+++ discord/bot.py
@@ -13,6 +13,7 @@ import discord
 # from typing import Optional
 from dotenv import load_dotenv
 from discord import app_commands
+from discord.ext import commands
 
 
 load_dotenv()
@@ -27,12 +28,12 @@ if not DISCORD_TOKEN:
 # pylint: disable=redefined-outer-name
 
 
-class MyClient(discord.Client):
+class MyBot(commands.Bot):
     """Main bot class"""
 
     def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
+        super().__init__(command_prefix='$', intents=intents)
+        # self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
         # This copies the global commands over to your guild.
@@ -41,23 +42,25 @@ class MyClient(discord.Client):
 
 
 intents = discord.Intents.default()
-client = MyClient(intents=intents)
+intents.message_content = True
+bot = MyBot(intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
     """Fires this function when bot is ready"""
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    await bot.load_extension('jishaku')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
 
-@client.tree.command()
+@bot.tree.command()
 async def hello(interaction: discord.Interaction):
     """Says hello!"""
     await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
 
-@client.tree.command()
+@bot.tree.command()
 @app_commands.describe(username="The PIMD username who's misc you wanted to see")
 async def misc(interaction: discord.Interaction, username: str):
     """Calculates misc and returns"""
@@ -67,8 +70,9 @@ async def misc(interaction: discord.Interaction, username: str):
 
     path = pathlib.Path(os.getcwd()) / '..'
 
+    python = path / 'venv' / 'bin' / 'python3'
     try:
-        c = await loop.run_in_executor(None, functools.partial(subprocess.run, ['py', 'misc_fetcher.py', '-Hu', username],
+        c = await loop.run_in_executor(None, functools.partial(subprocess.run, [python, 'misc_fetcher.py', '-Hu', username],
                                                                capture_output=True, encoding='utf-8', cwd=path))
 
     # pylint: disable=broad-exception-caught
@@ -79,7 +83,7 @@ async def misc(interaction: discord.Interaction, username: str):
     await interaction.followup.send(f"```\n{c.stdout}```")
 
 
-@client.tree.command()
+@bot.tree.command()
 @app_commands.describe(hits="Total number of hits done", percentage="What percent cash is being taken", total="Total cash taken")
 async def hits(interaction: discord.Interaction, hits: int, percentage: float, total: str):
     """Calculates the cash lost and left"""
@@ -88,9 +92,9 @@ async def hits(interaction: discord.Interaction, hits: int, percentage: float, t
     await interaction.response.defer()
 
     path = pathlib.Path(os.getcwd()) / '..'
-
+    python = path / 'venv' / 'bin' / 'python3'
     try:
-        c = await loop.run_in_executor(None, functools.partial(subprocess.run, ['py', 'hits.py', str(hits), str(percentage), total],
+        c = await loop.run_in_executor(None, functools.partial(subprocess.run, [python, 'hits.py', str(hits), str(percentage), total],
                                                                capture_output=True, encoding='utf-8', cwd=path))
 
     # pylint: disable=broad-exception-caught
@@ -101,7 +105,7 @@ async def hits(interaction: discord.Interaction, hits: int, percentage: float, t
     await interaction.followup.send(f"```\n{c.stdout}```")
 
 
-@client.tree.command()
+@bot.tree.command()
 @app_commands.describe(ally="Comma seperated list of allies to add.")
 async def add_ally(interaction: discord.Interaction, ally: str):
     """Adds allies to the database"""
@@ -109,10 +113,12 @@ async def add_ally(interaction: discord.Interaction, ally: str):
     loop = asyncio.get_event_loop()
     allies = [x.strip() for x in ally.split(',')]
 
-    cmd = ['py', 'add_ally.py', '-u']
-    cmd.extend(allies)
-
     path = pathlib.Path(os.getcwd()) / '..'
+
+    python = path / 'venv' / 'bin' / 'python3'
+
+    cmd = [python, 'add_ally.py', '-u']
+    cmd.extend(allies)
 
     await interaction.response.defer()
 
@@ -127,4 +133,4 @@ async def add_ally(interaction: discord.Interaction, ally: str):
 
     await interaction.followup.send(f"```\n{c.stdout}```")
 
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
