1d2307d1c7db63f027c0fbd806b0e122d3a48ffb
Gurnoor Singh <44425264+xGurnoor@users.noreply.github.com>
2024-08-28 02:38:43 +0530

    fix: binding error

--- discord/bot.py
+++ discord/bot.py
@@ -171,7 +171,7 @@ async def stop_watching(interaction: discord.Interaction, ally: str):
     await interaction.response.defer()
 
     cur = db.cursor()
-    res = cur.execute("SELECT * FROM allies WHERE username LIKE '?%'", (ally, ))
+    res = cur.execute("SELECT * FROM allies WHERE username LIKE ?", (ally + "%", ))
     r = res.fetchone()
     if not r:
         return interaction.followup.send(f'`{ally}` is not being watched.')
